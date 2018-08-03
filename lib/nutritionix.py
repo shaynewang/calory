415f28c821dfb93b3f8133709d5d8baa85af49c0
Shayne Wang <shaynexwang@gmail.com>
2018-08-02 22:21:54 -0700

    added method to return calories information

--- lib/nutritionix.py
+++ lib/nutritionix.py
@@ -14,7 +14,7 @@ class api:
         self.x_app_id  = os.getenv("NUTRITIONIX_APP_ID")
         self.x_app_key = os.getenv("NUTRITIONIX_KEY")
 
-    def search(self, food):
+    def search(self, food, mode="first branded"):
         """
         Search for food in nutritionix database
         then return json as string
@@ -28,15 +28,32 @@ class api:
         try:
             response = requests.get(request_url,headers=headers)
             if response.status_code == 200:
-                return response.text
+                # 20 branded and 20 common foods results are
+                # returned but we are only using the top result
+                if mode == "first common":
+                    # no calories information available
+                    return json.loads(response.text)["common"][0]
+                elif mode == "first branded":
+                    return json.loads(response.text)["branded"][0]
+                else:
+                    # maybe insert better logic to 
+                    # return more accurate search result
+                    raise Exception("Invalid mode.")
         except Exception as e:
             raise e
         return "error"
 
+    def get_calories(self, food, mode="first branded"):
+        """
+        Return food name and its calories per serving
+        """
+        query_result = self.search(food,mode)
+        return "{0}: {1} Cal per {2}".format(query_result["food_name"],
+                query_result["nf_calories"]/query_result["serving_qty"],
+                query_result["serving_unit"])
+
 if __name__ == "__main__":
     nxapi= api()
-    r = nxapi.search("pepperoni pizza")
-    print(r)
-    print(json.dumps(r))
-
-
+    print(nxapi.get_calories("pepperoni pizza"))
+    print(nxapi.get_calories("Godiva Chocolate"))
+    print(nxapi.get_calories("Granny Smith apple"))
