dc1ad478c0551361173c1bf57eb73bd7382004a9
Shayne Wang <shaynexwang@gmail.com>
2018-08-11 18:22:22 -0700

    increased speed with helps from minions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import threading
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sslify import SSLify
 from werkzeug.utils import secure_filename
@@ -27,6 +28,12 @@ def is_image_file(filename):
     Otherwise return False
     """
     return "." in filename and filename.split(".")[-1].lower() in IMG_EXTN
+
+def get_calories_threaded(food,food_info):
+    food_info.add(finfo.get_calories(food))
+
+def translate_threaded(i,lang,food_info):
+    food_info.add(trans.translate(i,lang))
 
 @app.route("/")
 def index():
@@ -65,8 +72,18 @@ def info(image):
         #os.remove(image_path)
     except:
         return redirect(url_for("upload"))
+    queue = []
+
+    # assign jobs to minions(Bob, Kevin, etc...)
     for food in set(foods):
-        food_info.add(finfo.get_calories(food))
+        minion = threading.Thread(target=get_calories_threaded, args=(food,food_info,))
+        queue.append(minion)
+        minion.start()
+
+    # collect results from the minions
+    for minion in queue:
+        minion.join()
+
     if '' in food_info: food_info.remove('')
     if not food_info or food_info == (''):
         food_info.add("No calories information found...")
@@ -79,12 +96,20 @@ def translated():
     Display translated information for foods
     """
     if request.method == "POST":
-        food_info = []
+        food_info = set()
         info = request.form["info"].split("\r\n")
         lang = request.form["lang"]
         image = request.form["image"]
         food_info_en = [i for i in info if i.strip()]
-        food_info = [trans.translate(i,lang) for i in food_info_en]
+        queue = []
+        for i in food_info_en:
+            minion = threading.Thread(target=translate_threaded, args=(i,lang,food_info,))
+            queue.append(minion)
+            minion.start()
+
+        for minion in queue:
+            minion.join()
+
         languages = trans.available_languages()
     return render_template("info.html",image=image, food_info=food_info, food_info_en=food_info_en, languages=languages)
 
